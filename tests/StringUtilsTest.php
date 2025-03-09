acc40d31c03e69839fe938fd43d74616f28192d0
ruzil <muftakhovruzil@yandex.ru>
2025-03-09 22:20:44 +0500

    tests 2nd lesson

--- tests/StringUtilsTest.php
+++ tests/StringUtilsTest.php
@@ -1,17 +1,22 @@
 <?php
-/*
-Нажмите кнопку run чтобы запустить тесты.
-Попробуйте изменять код функции / тестов, запуская проверки заново.
-*/
 
 require("src/StringUtils.php");
+require("vendor/autoload.php");
 
-if (StringUtils\capitalize('hello') !== 'Hello') {
-    throw new \Exception('Функция работает неверно!');
-}
+use Webmozart\Assert\Assert;
+use function StringUtils\capitalize;
 
-if (StringUtils\capitalize('') !== '') {
-    throw new \Exception('Функция работает неверно!');
-}
+Assert::eq(capitalize('hello'), 'Hello',"Тест не пройден");
+// Первый параметр actual – то, что пришло
+// Второй параметр expected – то, что ожидает тест
+// Правильный порядок аргументов имеет большое значение при анализе ошибки
+
+//if (StringUtils\capitalize('hello') !== 'Hello') {
+//    throw new \Exception('Функция работает неверно!');
+//}
+
+//if (StringUtils\capitalize('') !== '') {
+//    throw new \Exception('Функция работает неверно!');
+//}
 
 echo 'Все тесты пройдены!';
